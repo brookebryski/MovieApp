fe6e2ca6d311107d50b1e70e729558c69ff56a8c
BrookeBryski <brookeabryski@gmail.com>
2024-02-14 13:44:43 -0800

    basic razor / ef scaffolding

---
+++ MovieApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Data;
+using MovieApp.Models;
+
+namespace MovieApp.Pages_Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MovieApp.Data.MovieAppContext _context;
+
+        public IndexModel(MovieApp.Data.MovieAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
